b4e446cf8aa0ce61d2140496afba03e6c2ee2bba
Tate Shepard <tateshepard@yahoo.com>
2019-07-26 00:03:39 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
-# Add your variables here
+# this is where the code needs to be written to pass.
+# expects the first number to be an integer or a Float
+
+first_number = 7
+second_number = 8
+sum =  15
+difference = -1
+product = first_number * second_number
+quotient = first_number / second_number
